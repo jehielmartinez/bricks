571f1b75b6713e8c65a0ccb9d64a50add77d05f7
jehielmartinez <jehiel.martinez@codexitos.com>
2023-04-22 10:58:35 -0600

    chore: hanlde paddle movement

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ game_over = False
 # Paddle
 paddle = pygame.image.load("./images/paddle.png").convert_alpha()
 paddle_rect = paddle.get_rect()
+paddle_rect[1] = screen.get_height() - paddle_rect[3]
 
 # Ball
 ball = pygame.image.load("./images/football.png").convert_alpha()
@@ -18,6 +19,20 @@ ball_rect = ball.get_rect()
 # Bricks
 brick = pygame.image.load("./images/brick.png").convert_alpha()
 brick_rect = brick.get_rect()
+bricks = []
+bricks_gap = 5
+brick_plus_gap = brick_rect[2] + bricks_gap
+brick_rows = 5
+brick_cols = (screen.get_width() // brick_plus_gap)
+
+side_gap = (screen.get_width() - (brick_cols * brick_plus_gap) + bricks_gap) // 2
+
+for y in range(brick_rows):
+    brickY = y * (brick_rect[3] + bricks_gap)
+    for x in range(brick_cols):
+        brickX = x * (brick_rect[2] + bricks_gap) + side_gap
+        bricks.append((brickX, brickY))
+       
 
 
 def check_quit():
@@ -26,9 +41,30 @@ def check_quit():
             return True
     return False
 
-while not game_over:
-    dt = clock.tick(60)
-    screen.fill((0, 0, 0))
-    game_over = check_quit()
+def draw_bricks():
+    for b in bricks:
+        screen.blit(brick, b)
 
+def draw_paddle():
+    screen.blit(paddle, paddle_rect)
+
+def move_paddle(dt):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        paddle_rect[0] -= 5 * dt
+    if keys[pygame.K_RIGHT]:
+        paddle_rect[0] += 5 * dt
+    if paddle_rect[0] < 0:
+        paddle_rect[0] = 0
+    if paddle_rect[0] > screen.get_width() - paddle_rect[2]:
+        paddle_rect[0] = screen.get_width() - paddle_rect[2]
+
+while not game_over:
+  dt = clock.tick(60)
+  screen.fill((0, 0, 0))
+  draw_bricks()
+  draw_paddle()
+  move_paddle(dt)
+  game_over = check_quit()
+  pygame.display.update()
 pygame.quit()

7e68f986dedea51858958cacb9bde9f0cbc07523
jehielmartinez <jehiel.martinez@codexitos.com>
2023-04-22 11:38:50 -0600

    chore: moving and bouncing the ball

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ paddle_rect[1] = screen.get_height() - paddle_rect[3]
 # Ball
 ball = pygame.image.load("./images/football.png").convert_alpha()
 ball_rect = ball.get_rect()
+ball_start = (screen.get_width() // 2, screen.get_height() // 1)
+ball_speed = (-0.3, -0.3)
+ball_served = False
+sx, sy = ball_speed
+ball_rect.topleft = ball_start
 
 # Bricks
 brick = pygame.image.load("./images/brick.png").convert_alpha()
@@ -35,7 +40,7 @@ for y in range(brick_rows):
        
 
 
-def check_quit():
+def handle_quit():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return True
@@ -59,12 +64,48 @@ def move_paddle(dt):
     if paddle_rect[0] > screen.get_width() - paddle_rect[2]:
         paddle_rect[0] = screen.get_width() - paddle_rect[2]
 
+def draw_ball():
+    screen.blit(ball, ball_rect)
+
+def move_ball(dt, ball_served, sx, sy):
+    if ball_served:
+        ball_rect[0] += sx * dt
+        ball_rect[1] += sy * dt
+    # if bounce left
+    if ball_rect[0] <= 0:
+        ball_rect[0] = 0
+        sx *= -1
+    # if bounce right
+    if ball_rect[0] >= screen.get_width() - ball_rect[2]:
+        ball_rect[0] = screen.get_width() - ball_rect[2]
+        sx *= -1
+    # if bounce top
+    if ball_rect[1] <= 0:
+        ball_rect[1] = 0
+        sy *= -1
+    # if bounce bottom
+    if ball_rect[1] >= screen.get_height() - ball_rect[3]:
+        ball_rect[1] = screen.get_height() - ball_rect[3]
+        sy *= -1
+    return sx, sy
+
+def handle_serve(current_serve):
+    if current_serve:
+        return True
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        return True
+    return False
+
 while not game_over:
-  dt = clock.tick(60)
+  dt = clock.tick(50)
   screen.fill((0, 0, 0))
   draw_bricks()
+  draw_ball()
   draw_paddle()
   move_paddle(dt)
-  game_over = check_quit()
+  sx, sy = move_ball(dt, ball_served, sx, sy)
+  ball_served = handle_serve(ball_served)
+  game_over = handle_quit()
   pygame.display.update()
 pygame.quit()

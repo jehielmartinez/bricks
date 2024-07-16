6859e8d92d807f48ec4363e331dd0f182c7c4584
jehielmartinez <jehielmartinez@gmail.com>
2024-07-16 16:12:06 -0600

    chore: Add pygame to requirements.txt

--- main.py
+++ main.py
@@ -1,148 +1,98 @@
 import pygame
+from pygame.locals import QUIT, KEYDOWN, K_LEFT, K_RIGHT, K_SPACE
 
+# Initialization
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Bricks")
-
 clock = pygame.time.Clock()
-game_over = False
 
-# Paddle
-paddle = pygame.image.load("./images/paddle.png").convert_alpha()
-paddle_rect = paddle.get_rect()
-paddle_rect[1] = screen.get_height() - paddle_rect[3]
-paddle_rect[0] = screen.get_width() // 2 - paddle_rect[2] // 2
+# Load images
+paddle_img = pygame.image.load("./images/paddle.png").convert_alpha()
+ball_img = pygame.image.load("./images/football.png").convert_alpha()
+brick_img = pygame.image.load("./images/brick.png").convert_alpha()
 
-# Ball
-ball = pygame.image.load("./images/football.png").convert_alpha()
-ball_rect = ball.get_rect()
-ball_start = (screen.get_width() // 2, screen.get_height() // 2)
-ball_speed = (0.3, 0.3)
+# Paddle setup
+paddle_rect = paddle_img.get_rect(midbottom=(screen.get_width() // 2, screen.get_height() - 10))
+
+# Ball setup
+ball_rect = ball_img.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+ball_speed = [300, 300]  # pixels per second
 ball_served = False
-ball_rect.topleft = ball_start
 
-# Bricks
-brick = pygame.image.load("./images/brick.png").convert_alpha()
-brick_rect = brick.get_rect()
-bricks = []
-bricks_gap = 5
-brick_plus_gap = brick_rect[2] + bricks_gap
+# Brick setup
+brick_rect = brick_img.get_rect()
+brick_gap = 5
 brick_rows = 5
-brick_cols = (screen.get_width() // brick_plus_gap)
+brick_cols = screen.get_width() // (brick_rect.width + brick_gap)
+side_gap = (screen.get_width() - (brick_cols * (brick_rect.width + brick_gap))) // 2
 
-side_gap = (screen.get_width() - (brick_cols * brick_plus_gap) + bricks_gap) // 2
-
-for y in range(brick_rows):
-    brickY = y * (brick_rect[3] + bricks_gap)
-    for x in range(brick_cols):
-        brickX = x * (brick_rect[2] + bricks_gap) + side_gap
-        bricks.append((brickX, brickY))
-
-# Game State
-game_state = {
-    "ball_served": False,
-    "ball_rect": ball_rect,
-    "dt": 0,
-    "paddle_rect": paddle_rect,
-    "sx": ball_speed[0],
-    "sy": ball_speed[1],
-}
-       
-def handle_quit():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return True
-    return False
+bricks = [(x * (brick_rect.width + brick_gap) + side_gap, y * (brick_rect.height + brick_gap))
+          for y in range(brick_rows) for x in range(brick_cols)]
 
 def draw_bricks():
-    for b in bricks:
-        screen.blit(brick, b)
+    for brick_pos in bricks:
+        screen.blit(brick_img, brick_pos)
 
-def draw_paddle():
-    screen.blit(paddle, game_state["paddle_rect"])
+def handle_events():
+    global ball_served
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            return True
+        if event.type == KEYDOWN and event.key == K_SPACE:
+            ball_served = True
+    return False
 
-def move_paddle():
-    if game_state["ball_served"]:
-      keys = pygame.key.get_pressed()
-      if keys[pygame.K_LEFT]:
-          game_state["paddle_rect"][0] -= 1 * game_state["dt"]
-      if keys[pygame.K_RIGHT]:
-          game_state["paddle_rect"][0] += 1 * game_state["dt"]
-      if game_state["paddle_rect"][0] < 0:
-          game_state["paddle_rect"][0] = 0
-      if game_state["paddle_rect"][0] > screen.get_width() - game_state["paddle_rect"][2]:
-          game_state["paddle_rect"][0] = screen.get_width() - game_state["paddle_rect"][2]
-
-def draw_ball():
-    screen.blit(ball, game_state["ball_rect"])
-
-def move_ball():
+def move_paddle(dt):
     keys = pygame.key.get_pressed()
-    if game_state["ball_served"]:
-        game_state["ball_rect"][0] += game_state["sx"] * game_state["dt"]
-        game_state["ball_rect"][1] += game_state["sy"] * game_state["dt"]
-    # if bounce left
-    if game_state["ball_rect"][0] <= 0:
-        game_state["ball_rect"][0] = 0
-        game_state["sx"] *= -1
-    # if bounce right
-    if game_state["ball_rect"][0] >= screen.get_width() - game_state["ball_rect"][2]:
-        game_state["ball_rect"][0] = screen.get_width() - game_state["ball_rect"][2]
-        game_state["sx"] *= -1
-    # if bounce top
-    if game_state["ball_rect"][1] <= 0:
-        game_state["ball_rect"][1] = 0
-        game_state["sy"] *= -1
-    # if bounce bottom
-    if game_state["ball_rect"][1] >= screen.get_height() - game_state["ball_rect"][3]:
-        # set initial state
-        game_state["ball_served"] = False
-        game_state["ball_rect"] = ball_rect
-        game_state["sx"] = ball_speed[0]
-        game_state["sy"] = ball_speed[1]
-        game_state["paddle_rect"] = paddle_rect
-        game_state["ball_rect"].topleft = ball_start
-    # if bounce paddle
-    if ball_rect.colliderect(paddle_rect):
-        game_state["ball_rect"][1] = paddle_rect[1] - game_state["ball_rect"][3]
-        game_state["sy"] *= -1
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            game_state["sx"] -= 0.01
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            game_state["sx"] += 0.01
-        increase_dificulty(0.0001)
-    # if bounce brick
-    for b in bricks:
-        if game_state["ball_rect"].colliderect(pygame.Rect(b[0], b[1], brick_rect[2], brick_rect[3])):
-            bricks.remove(b)
-            game_state["sy"] *= -1
-            break
+    if keys[K_LEFT]:
+        paddle_rect.x -= 500 * dt
+    if keys[K_RIGHT]:
+        paddle_rect.x += 500 * dt
+    paddle_rect.clamp_ip(screen.get_rect())
 
-def handle_serve():
-    if game_state["ball_served"]:
-        game_state["ball_served"] = True
-        return
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        game_state["ball_served"] = True
-        return
-    game_state["ball_served"] = False
+def move_ball(dt):
+    global ball_served
+    if ball_served:
+        ball_rect.x += ball_speed[0] * dt
+        ball_rect.y += ball_speed[1] * dt
 
-def increase_dificulty(delta = 0.1):
-    if game_state["sy"] > 0:
-        game_state["sy"] += delta
-    else:
-        game_state["sy"] -= delta
+        if ball_rect.left <= 0 or ball_rect.right >= screen.get_width():
+            ball_speed[0] *= -1
+        if ball_rect.top <= 0:
+            ball_speed[1] *= -1
+        if ball_rect.colliderect(paddle_rect):
+            ball_rect.bottom = paddle_rect.top
+            ball_speed[1] *= -1
+            keys = pygame.key.get_pressed()
+            if keys[K_LEFT]:
+                ball_speed[0] -= 10
+            if keys[K_RIGHT]:
+                ball_speed[0] += 10
 
-while not game_over:
-  game_state["dt"] = clock.tick(50)
-  screen.fill((0, 0, 0))
-  handle_serve()
-  draw_bricks()
-  draw_ball()
-  draw_paddle()
-  move_paddle()
-  move_ball()
-  game_over = handle_quit()
-  pygame.display.update()
+        for brick_pos in bricks[:]:
+            brick_rect.topleft = brick_pos
+            if ball_rect.colliderect(brick_rect):
+                bricks.remove(brick_pos)
+                ball_speed[1] *= -1
+                break
+
+        if ball_rect.bottom >= screen.get_height():
+            ball_served = False
+            ball_rect.center = (screen.get_width() // 2, screen.get_height() // 2)
+            ball_speed[:] = [300, 300]
+
+# Main game loop
+game_running = True
+while game_running:
+    dt = clock.tick(60) / 1000  # seconds since last frame
+    screen.fill((0, 0, 0))
+    game_running = not handle_events()
+    draw_bricks()
+    screen.blit(ball_img, ball_rect)
+    screen.blit(paddle_img, paddle_rect)
+    move_paddle(dt)
+    move_ball(dt)
+    pygame.display.flip()
+
 pygame.quit()
